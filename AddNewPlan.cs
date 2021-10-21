8c05bd3fb6323a4c64c9db521d0ebe3ccf0fb790
Eating Wu <114040@ntuh.gov.tw>
2021-10-21 16:40:01 +0800

    Add markers and body, remaining Add new plan+Beam for new isocenter

--- AddNewPlan.cs
+++ AddNewPlan.cs
@@ -12,8 +12,8 @@ using AddNewPlan;
 
 
 // TODO: Replace the following version attributes by creating AssemblyInfo.cs. You can do this in the properties of the Visual Studio project.
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
 [assembly: AssemblyInformationalVersion("1.0")]
 
 // TODO: Uncomment the following line if the script requires write access.
@@ -33,7 +33,7 @@ namespace VMS.TPS
             UserControl1 userControl = new UserControl1(scriptcontext);
             window.Content = userControl;
             window.Title = "AutoAddBeam";
-            window.Height = 165;
+            window.Height = 300;
             window.Width = 300;
         }
     }
--- UserControl1.xaml.cs
+++ UserControl1.xaml.cs
@@ -25,16 +25,21 @@ namespace AddNewPlan
     {
         public List<String> MachineName { get; set; }
         public ScriptContext SC { get; set; }
+        public VMS.TPS.Common.Model.API.Image SI { get; set; }
+        public StructureSet SS { get; set; }
+        public List<String> Markers { get; set; }
         public UserControl1(ScriptContext scriptContext)
         {
             SC = scriptContext;
+            SS = SC.StructureSet;
+            Markers = new List<string>();
 
             MachineName = new List<String>();
             MachineName.Add("LA3TB1623");
             MachineName.Add("LA5TB2069");
             MachineName.Add("LA6TB4313");
             MachineName.Add("LA7TB4557");
-            //MachineName.Add("iX 1100");
+            MachineName.Add("iX 1100");
 
             InitializeComponent();
             DataContext = this;
@@ -42,7 +47,7 @@ namespace AddNewPlan
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             SC.Patient.BeginModifications();
-
+            SI = SC.Image;
             Course course = SC.Patient.Courses.Where(s => s.Id =="AutoLoad").FirstOrDefault();
             if (course is null)
             {
@@ -50,20 +55,45 @@ namespace AddNewPlan
                 course.Id = "AutoLoad";
             }
 
-            StructureSet ss = SC.StructureSet;
+            StructureSet ss = SC.Patient.StructureSets.Where(s => s.Id == SI.Id).FirstOrDefault();
+            if (ss is null)
+            {
+                ss = SI.CreateNewStructureSet();
+                ss.Id = SI.Id;
+            }
             ExternalPlanSetup plan = course.AddExternalPlanSetup(ss);
+
             if (ss.Id.Length < 9) { plan.Id = "Load" + ss.Id; } else { plan.Id = ss.Id; }
+            var BodyPar = ss.GetDefaultSearchBodyParameters();
+            ss.CreateAndSearchBody(BodyPar);
 
             string LINACID = machine.SelectedItem.ToString();
             ExternalBeamMachineParameters beamparams = new ExternalBeamMachineParameters(LINACID, "6X", 600, "STATIC", null);
-            VVector isocenter = new VVector(Math.Round(SC.Image.UserOrigin.x, 0), Math.Round(SC.Image.UserOrigin.y, 0), Math.Round(SC.Image.UserOrigin.z, 0));
+            VVector isocenter = new VVector(SC.Image.UserOrigin.x, SC.Image.UserOrigin.y, SC.Image.UserOrigin.z);
             plan.AddMLCBeam(beamparams, null, new VRect<double>(-50, -50, 50, 50), 0, 0, 0, isocenter);
+
             var myDRR = new DRRCalculationParameters(500); // 500mm is the DRR size
             myDRR.SetLayerParameters(0, 0.1, -550, 0, -100, 100); // Layer 1
             myDRR.SetLayerParameters(1, 0.9, 100, 1000, -100, 100); // Layer 2
+
             foreach (Beam beam in plan.Beams)
             { beam.CreateOrReplaceDRR(myDRR);}
+
             Window.GetWindow(this).Close();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            Markers.Add("User Origin\tx:" + SC.Image.UserOrigin.x + "\ty:" + SC.Image.UserOrigin.y + "\tz:" + SC.Image.UserOrigin.y);
+            foreach(Structure a in SS.Structures.Where(s => s.DicomType == "MARKER"))
+            {
+                Markers.Add("\n" + a.Id + "\tx:"+ a.CenterPoint.x + "\ty:" + a.CenterPoint.y+ "\tz:" + a.CenterPoint.z);
+            }
+        }
+        
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Markers.Clear();
         }
     }
 
